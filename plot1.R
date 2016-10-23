5ab10d9b13dfd53b3844de785a6497bea4779100
Aiganym Baizakova <aiganym.baizakova@gmail.com>
2016-10-23 16:13:15 +0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+if(!file.exists('EDA')) dir.create('EDA')
+> fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+> download.file(fileUrl, destfile = './EDA/household_power_consumption.zip')
+trying URL 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+Content type 'application/zip' length 20640916 bytes (19.7 MB)
+downloaded 19.7 MB
+
+> unzip('./EDA/household_power_consumption.zip', exdir = './EDA')
+> 
+> fileEDA  <- file('./EDA/household_power_consumption.txt')
+
+> dataEDA <- read.table(text = grep("^[1,2]/2/2007",readLines(fileEDA),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+> 
+
+> if(!file.exists('my_plots_EDA')) dir.create('my_plots_EDA')
+> png(filename = './my_plots_EDA/plot1.png', width = 480, height = 480, units='px')
+> with(dataEDA, hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
+> dev.off
